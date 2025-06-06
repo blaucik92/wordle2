dc0663b8e06c78594558c3f0ea8760f03d774956
Golebioo <golebioo@bswiesaude.onmicrosoft.com>
2025-06-06 09:50:51 +0200

    - losowe haslo dziala

--- app/src/main/java/informatik/campus/wordle/MainActivity.java
+++ app/src/main/java/informatik/campus/wordle/MainActivity.java
@@ -12,14 +12,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 /*
 -TODO
--- Array z haslami
+-- Array z haslami (dziala z arraya zostaje wybrane jedno haslo za kazdym startem aplikacji)
 -- rozczlonkowanie arraya na piec liter zeby wszystko sprawdzic
 -- sprawdzanie hasla
 -- zmiana widocznosci na kolejny wiersz
--- moze jak sie uda i nie bedzie zbyt skomplikowane po wpisaniu jeden litery od razu przeskakuje dalej
+-- moze jak sie uda i nie bedzie zbyt skomplikowane to po wpisaniu jednej litery od razu przeskakuje dalej
 */
 
     @Override
@@ -33,14 +35,60 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         EditText ET3 = findViewById(R.id.ET3);
         EditText ET4 = findViewById(R.id.ET4);
         EditText ET5 = findViewById(R.id.ET5);
+        EditText ET6 = findViewById(R.id.ET6);
+        EditText ET7 = findViewById(R.id.ET7);
+        EditText ET8 = findViewById(R.id.ET8);
+        EditText ET9 = findViewById(R.id.ET9);
+        EditText ET10 = findViewById(R.id.ET10);
+        EditText ET11 = findViewById(R.id.ET11);
+        EditText ET12 = findViewById(R.id.ET12);
+        EditText ET13 = findViewById(R.id.ET13);
+        EditText ET14 = findViewById(R.id.ET14);
+        EditText ET15 = findViewById(R.id.ET15);
+        EditText ET16 = findViewById(R.id.ET16);
+        EditText ET17 = findViewById(R.id.ET17);
+        EditText ET18 = findViewById(R.id.ET18);
+        EditText ET19 = findViewById(R.id.ET19);
+        EditText ET20 = findViewById(R.id.ET20);
+        EditText ET21 = findViewById(R.id.ET21);
+        EditText ET22 = findViewById(R.id.ET22);
+        EditText ET23 = findViewById(R.id.ET23);
+        EditText ET24 = findViewById(R.id.ET24);
+        EditText ET25 = findViewById(R.id.ET25);
+        EditText ET26 = findViewById(R.id.ET26);
+        EditText ET27 = findViewById(R.id.ET27);
+        EditText ET28 = findViewById(R.id.ET28);
+        EditText ET29 = findViewById(R.id.ET29);
+        EditText ET30 = findViewById(R.id.ET30);
+
+        TextView testHasla = findViewById(R.id.HasloTest);
 
 
 
         Button btnPruefen = findViewById(R.id.btnPruefen);
-
-
-
         btnPruefen.setOnClickListener(this);
+
+
+
+
+        String[] naturWorter = {
+                "WIESE", "BLUME", "ERDEN", "BÄUME", "FLUSS",
+                "WELLE", "NEBEL", "STEIN", "GRUBE", "ASTEN",
+                "BERGE", "HÖHLE", "EICHE", "RINDE", "SUMPF"
+        };
+
+
+
+        String zufall = naturWorter[new Random().nextInt(naturWorter.length)];
+
+        char[] buchstaben = zufall.toCharArray();
+
+
+        testHasla.setText(zufall);
+
+        ET1.setText(buchstaben[1]);
+        ET2.setText(buchstaben[0]);
+        ET3.setText(buchstaben[4]);
 
 
 
@@ -55,7 +103,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
-        String dziala = "dziala";
+
 
     }
 }
